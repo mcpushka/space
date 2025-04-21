fb029ed6702919df576cf6b6e62908c8d8adf7aa
mcpushka <ssp.prog@mail.ru>
2025-04-21 10:01:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.150.1/examples/jsm/controls/OrbitControls.js';
+import * as THREE        from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r150/three.module.min.js';
+import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r150/examples/jsm/controls/OrbitControls.js';
+
 
 // Сцена, камера и рендерер
 const scene = new THREE.Scene();
